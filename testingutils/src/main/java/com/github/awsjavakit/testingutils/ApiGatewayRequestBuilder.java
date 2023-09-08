0e93db5201e3ef6b095b0ac339bbb79685372ea9
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2023-09-08 15:28:23 +0200

    actually move file

--- testingutils/src/main/java/com/github/awsjavakit/testingutils/ApiGatewayRequestBuilder.java
+++ testingutils/src/main/java/com/github/awsjavakit/testingutils/ApiGatewayRequestBuilder.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ApiGatewayRequestBuilder {
+
   private final ObjectMapper objectMapper;
   private final ApiGatewayEvent event;
 
