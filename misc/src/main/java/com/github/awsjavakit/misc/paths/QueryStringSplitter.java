13c5460bbbbe07d44d2605090e10fa3dcadd3692
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2023-10-23 08:43:51 +0200

    fix typo

--- misc/src/main/java/com/github/awsjavakit/misc/paths/QueryStringSplitter.java
+++ misc/src/main/java/com/github/awsjavakit/misc/paths/QueryStringSplitter.java
@@ -11,7 +11,7 @@ class QueryStringSplitter {
   public static final String CATCH_AMPERSAND_WHEN_NOT_ESCAPED = "(?<!\\\\)&";
   public static final int PROPERTY_PART = 0;
   public static final int VALUE_PART = 1;
-  public static final String ASSIGNMEN_OPERATOR = "=";
+  public static final String ASSIGNMENT_OPERATOR = "=";
   private final URI uri;
 
   public QueryStringSplitter(URI uri) {
@@ -22,7 +22,7 @@ class QueryStringSplitter {
     var queryString = uri.getQuery();
     return
       Arrays.stream(queryString.split(CATCH_AMPERSAND_WHEN_NOT_ESCAPED))
-        .map(str -> str.split(ASSIGNMEN_OPERATOR))
+        .map(str -> str.split(ASSIGNMENT_OPERATOR))
         .map(array -> Map.entry(array[PROPERTY_PART], array[VALUE_PART]))
         .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
 
