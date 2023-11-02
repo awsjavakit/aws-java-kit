160d08214321662514296c19d135659bc75598ee
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2023-11-02 14:10:55 +0100

    add iterator class

---
+++ misc/src/main/java/com/github/awsjavakit/misc/lists/PartitionedListIterator.java
@@ -0,0 +1,28 @@
+package com.github.awsjavakit.misc.lists;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class PartitionedListIterator<T> implements Iterator<List<T>> {
+
+  private final PartitionedList<T> contents;
+  private int currentIndex;
+
+  PartitionedListIterator(PartitionedList<T> contents) {
+    this.contents = contents;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return currentIndex < contents.size();
+  }
+
+  @Override
+  public List<T> next() {
+    var output = contents.get(currentIndex);
+    currentIndex++;
+    return output;
+
+  }
+
+}
