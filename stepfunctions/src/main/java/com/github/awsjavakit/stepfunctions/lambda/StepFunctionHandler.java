94d3d9bb73864bf2d7b749c829f0bf854e8ca75e
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2024-02-14 10:42:39 +0100

    lambda step function parses input with customer object mapper

--- stepfunctions/src/main/java/com/github/awsjavakit/stepfunctions/lambda/StepFunctionHandler.java
+++ stepfunctions/src/main/java/com/github/awsjavakit/stepfunctions/lambda/StepFunctionHandler.java
@@ -1,14 +1,39 @@
 package com.github.awsjavakit.stepfunctions.lambda;
 
+import static com.gtihub.awsjavakit.attempt.Try.attempt;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
-public class StepFunctionHandler implements RequestStreamHandler {
+public class StepFunctionHandler<I> implements RequestStreamHandler {
+
+  private final Class<I> inputClass;
+  private final ObjectMapper objectMapper;
+
+  public StepFunctionHandler(Class<I> inputClass, ObjectMapper objectMapper) {
+    this.inputClass = inputClass;
+    this.objectMapper = objectMapper;
+  }
 
   @Override
-  public void handleRequest(InputStream input, OutputStream output, Context context) {
-     throw new UnsupportedOperationException("Not implemented yet");
+  public void handleRequest(InputStream inputStream, OutputStream output, Context context)
+    throws IOException {
+    var input = attempt(() -> objectMapper.readValue(inputStream, inputClass)).orElseThrow();
+    writeOutput(input, output);
+
+  }
+
+  private void writeOutput(I input, OutputStream output) throws IOException {
+    try (var writer = new BufferedWriter(new OutputStreamWriter(output))) {
+      var json = attempt(() -> objectMapper.writeValueAsString(input)).orElseThrow();
+      writer.write(json);
+      writer.flush();
+    }
   }
 }
