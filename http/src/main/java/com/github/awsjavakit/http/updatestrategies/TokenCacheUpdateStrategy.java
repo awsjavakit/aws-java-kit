bb8a4e11206d5a25051840d71388cf9103196e18
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2024-02-15 13:21:03 +0100

    linting

--- http/src/main/java/com/github/awsjavakit/http/updatestrategies/TokenCacheUpdateStrategy.java
+++ http/src/main/java/com/github/awsjavakit/http/updatestrategies/TokenCacheUpdateStrategy.java
@@ -4,26 +4,26 @@ import com.github.awsjavakit.http.token.OAuthTokenEntry;
 import java.util.function.Supplier;
 
 /**
- *  Given a function that provides a new Token and updates some cache (updateCache),
- *  and a function that provides an already cached entry,
- *  then this function decides how will these functions will be combined to provide a valid token.
- *  Examples:
- *  <ul>
- *  <li>
+ * Given a function that provides a new Token and updates some cache (updateCache), and a function
+ * that provides an already cached entry, then this function decides how these functions will be
+ * combined to provide a valid token. Examples:
+ * <ul>
+ * <li>
  *
- *  <pre>{@code
- *   public OAuthTokenEntry fetchAndUpdate(
- *     Supplier<OAuthTokenEntry> fetchCachedEntry,
- *     Supplier<OAuthTokenEntry> updateCache) {
- *     return Optional.ofNullable(()->fetchCachedEntry.get())
- *        orEleGet(()->updateCache.get())
- *   }
- *  }</pre>
- *  </li>
- *  <li>{@link DefaultTokenCacheUpdateStrategy} </li>
- *  </ul>
+ * <pre>{@code
+ *  public OAuthTokenEntry fetchAndUpdate(
+ *    Supplier<OAuthTokenEntry> fetchCachedEntry,
+ *    Supplier<OAuthTokenEntry> updateCache) {
+ *    return Optional.ofNullable(()-> fetchCachedEntry.get())
+ *       orEleGet(()-> updateCache.get())
+ *  }
+ * }</pre>
+ * </li>
+ * <li>{@link DefaultTokenCacheUpdateStrategy} </li>
+ * </ul>
  */
 public interface TokenCacheUpdateStrategy {
 
-  OAuthTokenEntry fetchAndUpdate(Supplier<OAuthTokenEntry> fetchCachedEntry, Supplier<OAuthTokenEntry> updateCache);
+  OAuthTokenEntry fetchAndUpdate(Supplier<OAuthTokenEntry> fetchCachedEntry,
+    Supplier<OAuthTokenEntry> updateCache);
 }
