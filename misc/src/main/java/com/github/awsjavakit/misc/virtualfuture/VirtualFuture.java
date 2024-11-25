b97ebb01d7c47948999e55980fecaaec888e3343
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2024-11-25 10:28:32 +0100

    Add mapping function to virtual future

--- misc/src/main/java/com/github/awsjavakit/misc/virtualfuture/VirtualFuture.java
+++ misc/src/main/java/com/github/awsjavakit/misc/virtualfuture/VirtualFuture.java
@@ -3,19 +3,18 @@ package com.github.awsjavakit.misc.virtualfuture;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public final class VirtualFuture<A> {
 
 
   private final Supplier<A> task;
-  private final CompletableFuture<A> future;
+  private  CompletableFuture<A> future;
 
-  private VirtualFuture(Supplier<A> task) {
-    this.task = task;
-    this.future = new CompletableFuture<>();
-    Thread.startVirtualThread(() -> executeTaskInsideCompletableFuture(future));
-
+  private VirtualFuture(Supplier<A> inputTask) {
+    this.task=  inputTask;
+    execute();
   }
 
   public VirtualFuture(CompletableFuture<A> future) {
@@ -27,19 +26,39 @@ public final class VirtualFuture<A> {
     return new VirtualFuture<>(task);
   }
 
-  public static VirtualFuture<Void> allOf(VirtualFuture... futures) {
-    var completableFutures = Arrays.stream(futures).map(future -> future.future)
+    public static VirtualFuture<Void> allOf(VirtualFuture... futures) {
+    var completableFutures = Arrays.stream(futures)
+      .map(VirtualFuture::execute)
+      .map(future -> future.future)
       .toArray(CompletableFuture[]::new);
+
     var combinedFuture = CompletableFuture.allOf(completableFutures);
     return new VirtualFuture<Void>(combinedFuture);
   }
 
   public A join() {
+
     return this.future.join();
+  }
+
+  private VirtualFuture<A> execute() {
+    this.future = new CompletableFuture<>();
+    Thread.startVirtualThread(() -> executeTaskInsideCompletableFuture(future));
+    return this;
   }
 
   public A get() throws ExecutionException, InterruptedException {
     return future.get();
+  }
+
+  public <B> VirtualFuture<B> map(Function<A, B> function) {
+    var supplier = new Supplier<B>(){
+      @Override
+      public B get() {
+        return function.apply(task.get());
+      }
+    };
+    return VirtualFuture.supply(supplier);
   }
 
   private void executeTaskInsideCompletableFuture(CompletableFuture<A> future) {
