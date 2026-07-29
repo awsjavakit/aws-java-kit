f6aac506f174e5032e03fc3dc11d64d21b3bc07b
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2026-07-29 09:59:23 +0200

    inline variable

--- testingutils/src/main/java/com/github/awsjavakit/testingutils/aws/FakeSqsClient.java
+++ testingutils/src/main/java/com/github/awsjavakit/testingutils/aws/FakeSqsClient.java
@@ -101,10 +101,9 @@ public class FakeSqsClient implements SqsClient {
       .map(entry->Map.entry(entry.getKey(),stringToAttribute(entry.getValue())))
       .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
 
-    var result= Stream.of(messageAttributes.entrySet().stream(), messageSystemAttributes.entrySet().stream())
+    return Stream.of(messageAttributes.entrySet().stream(), messageSystemAttributes.entrySet().stream())
       .flatMap(s -> s)
       .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
-    return result;
   }
 
   private MessageAttribute stringToAttribute(String value) {
@@ -117,7 +116,7 @@ public class FakeSqsClient implements SqsClient {
   private MessageAttribute attributeValueToAttribute(MessageAttributeValue value) {
     var attribute = new MessageAttribute();
     attribute.setStringValue(value.stringValue());
-    attribute.setDataType("string");
+    attribute.setDataType(value.dataType());
     return attribute;
   }
 
